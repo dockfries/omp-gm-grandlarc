93f2dedc449f71371de7e2cd233fae3c2e93698b
dockfries <yucarl77@foxmail.com>
2025-10-11 14:35:40 +0800

    feat: update config and add NPC recordings, fix vehicle txt order

--- src/controllers/vehicle.ts
+++ src/controllers/vehicle.ts
@@ -2,7 +2,26 @@ import { Vehicle } from "@infernus/core";
 import fs from "fs";
 import path from "path";
 
-const vehicleFiles = path.resolve("scriptfiles/vehicles");
+const vehicleFolder = path.resolve("scriptfiles/vehicles");
+const vehicleFiles = [
+  "trains",
+  "pilots",
+  "lv_law",
+  "lv_airport",
+  "lv_gen",
+  "sf_law",
+  "sf_airport",
+  "sf_gen",
+  "ls_law",
+  "ls_airport",
+  "ls_gen_inner",
+  "ls_gen_outer",
+  "whetstone",
+  "bone",
+  "flint",
+  "tierra",
+  "red_county",
+];
 
 const LoadStaticVehiclesFromFile = (path: string) => {
   return new Promise<string[]>((resolve, reject) => {
@@ -15,29 +34,50 @@ const LoadStaticVehiclesFromFile = (path: string) => {
 
 export const loadAllStaticVehicles = async () => {
   try {
-    const files = fs.readdirSync(vehicleFiles);
-    const promiseList = files.map((fileName) => {
-      return LoadStaticVehiclesFromFile(path.join(vehicleFiles, fileName));
+    const promiseList = vehicleFiles.map((fileName) => {
+      return LoadStaticVehiclesFromFile(
+        path.join(vehicleFolder, fileName + ".txt")
+      );
     });
     const vehicleDataList = await Promise.all(promiseList);
+    let fileIndex = 0;
     let vehicles_loaded = 0;
     vehicleDataList.forEach((vehicles: string[]) => {
+      let index = 0;
       vehicles.forEach((vehicle: string) => {
+        index++;
+        if (vehicle.trim().length === 0) {
+          return;
+        }
         const realData = vehicle.substring(0, vehicle.indexOf(" ;")).split(",");
         const [modelId, spawnX, spawnY, spawnZ, spawnRot, color1, color2] =
           realData;
-        new Vehicle({
-          modelId: parseInt(modelId),
-          x: parseFloat(spawnX),
-          y: parseFloat(spawnY),
-          z: parseFloat(spawnZ),
-          zAngle: parseFloat(spawnRot),
-          color: [+color1, +color2],
-          respawnDelay: 30 * 60,
-          addSiren: false,
-        }).create(); // respawn 30 minutes
-        vehicles_loaded++;
+        const veh = new Vehicle(
+          {
+            modelId: parseInt(modelId),
+            x: parseFloat(spawnX),
+            y: parseFloat(spawnY),
+            z: parseFloat(spawnZ),
+            zAngle: parseFloat(spawnRot),
+            color: [+color1, +color2],
+            respawnDelay: 30 * 60,
+            addSiren: false,
+          },
+          true
+        ); // respawn 30 minutes
+        veh.create();
+        if (veh.id !== -1) {
+          vehicles_loaded++;
+        } else {
+          console.log(
+            "Failed to create vehicle",
+            realData,
+            vehicleFiles[fileIndex],
+            index
+          );
+        }
       });
+      fileIndex++;
     });
     return vehicles_loaded;
   } catch (err) {
