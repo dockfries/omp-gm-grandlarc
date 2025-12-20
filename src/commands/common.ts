67dcbc805c729514fd7a70807d9b0c6dc56fc0cc
dockfries <yucarl77@foxmail.com>
2025-12-20 14:29:00 +0800

    feat: kill after forceClassSelection

--- src/commands/common.ts
+++ src/commands/common.ts
@@ -2,16 +2,19 @@ import { playerSelections } from "@/controllers/selection";
 import { PlayerEvent } from "@infernus/core";
 
 PlayerEvent.onCommandText("kill", ({ player, next }) => {
+  if (!player.isSpawned()) return next();
   player.setHealth(0);
   return next();
 });
 
 PlayerEvent.onCommandText("changecity", ({ player, next }) => {
+  if (!player.isSpawned()) return next();
   const s = playerSelections.get(player)!;
 
   s.hasSelected = false;
   player.forceClassSelection();
-  player.toggleSpectating(true);
-  player.toggleSpectating(false);
+  player.setHealth(0);
+  // player.toggleSpectating(true);
+  // player.toggleSpectating(false);
   return next();
 });
